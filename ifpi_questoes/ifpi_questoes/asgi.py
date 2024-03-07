d12ef52fc16f15e10b8b366008b62ca117939a34
GiseleBCSantos <catce.2023111mtds0035@aluno.ifpi.edu.br>
2024-03-07 09:53:08 -0300

    First Commit

---
+++ ifpi_questoes/ifpi_questoes/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ifpi_questoes project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ifpi_questoes.settings')
+
+application = get_asgi_application()
