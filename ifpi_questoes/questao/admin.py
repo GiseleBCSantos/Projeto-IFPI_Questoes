a70ff60b7149c55c14aba5e23e0b8cb328b66cc4
GiseleBCSantos <catce.2023111mtds0035@aluno.ifpi.edu.br>
2024-03-07 10:50:18 -0300

    Criação models Area e Questao Adição usuário Isaac Adição 4 áreas

--- ifpi_questoes/questao/admin.py
+++ ifpi_questoes/questao/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Area, Questao
 
 # Register your models here.
+
+@admin.register(Area)
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome']
+    search_fields = ['nome']
+
+
+@admin.register(Questao)
+class QuestaoAdmin(admin.ModelAdmin):
+    list_display = ['ano', 'area']
+    search_fields = ['ano', 'area']
--- ifpi_questoes/questao/models.py
+++ ifpi_questoes/questao/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 # Create your models here.
+
+class Area(models.Model):
+    nome = models.CharField(max_length=40, null=False, blank=False)
+
+
+    class Meta:
+        verbose_name = 'Área'
+        verbose_name_plural = 'Áreas'
+        
+        
+    def __str__(self) -> str:
+        return self.nome
+    
+    
+class Questao(models.Model):
+    area = models.ForeignKey(Area, on_delete=models.CASCADE, related_name='area')
+    ano = models.IntegerField(choices=((i, i) for i in range(2009, 2024)))
+    contexto = models.CharField(max_length=300, null=False, blank=False)
+    pergunta = models.CharField(max_length=300, null=False, blank=False)
+    letra_a = models.CharField(max_length=300, null=False, blank=False)
+    letra_b = models.CharField(max_length=300, null=False, blank=False)
+    letra_c = models.CharField(max_length=300, null=False, blank=False)
+    letra_d = models.CharField(max_length=300, null=False, blank=False)
+    letra_e = models.CharField(max_length=300, null=False, blank=False)
+    answer = models.CharField(max_length=1, null=False, blank=False)
+    imagens = models.ImageField(upload_to='img_questoes', null=True, blank=True)
+    
+    
+    class Meta:
+        verbose_name = 'Questão'
+        verbose_name_plural = 'Questões'
+        
+    
+    def __str__(self) -> str:
+        return f'{self.ano}-{self.area}-{self.id}'
