3ccf6137d80ead7121fb70136afaa6481e7d0f11
Gisele <you@example.com>
2024-03-08 11:25:43 -0300

    Template com questoes

--- ifpi_questoes/questao/views.py
+++ ifpi_questoes/questao/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Questao
 
 # Create your views here.
+
+def index(request):
+    questoes = Questao.objects.all()
+    
+    context = {
+        'questoes': questoes
+    }
+    
+    return render(request, 'questao/index.html', context)
